6d912dfd321a24e0806e280e0dbedeaf7885c592
Farid Alam <faridalam62@gmail.com>
2023-06-13 01:02:51 +0600

    Updated instructor page

--- src/Components/InstructorCard.jsx
+++ src/Components/InstructorCard.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import photo from '../assets/class.jpg'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function InstructorCard() {
+function InstructorCard({ instructor }) {
+  const { name, email, role, photo } = instructor;
   return (
-    <div className='text-center'>
-        <img src={photo} className='mx-auto rounded-md'/>
-        <h2 className='text-xl mt-4'>Instructor Name</h2>
-        <p className='mb-4'>contact@gmail.com</p>
-        <Link to='/' className="bg-black text-white py-2 px-6 rounded-md text-sm">View Classes</Link>
+    <div className="text-center">
+      <img src={photo} className="mx-auto rounded-md" />
+      <h2 className="text-xl mt-4">{name}</h2>
+      <p className="mb-4">{email}</p>
+      <Link to="/" className="bg-black text-white py-2 px-6 rounded-md text-sm">
+        View Classes
+      </Link>
     </div>
-
-  )
+  );
 }
 
-export default InstructorCard
+export default InstructorCard;
--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import PageTitle from '../../Components/Shared/PageTitle'
-import cover from '../../assets/1.jpg'
-import InstructorCard from '../../Components/InstructorCard'
+import React, { useEffect, useState } from "react";
+import PageTitle from "../../Components/Shared/PageTitle";
+import cover from "../../assets/1.jpg";
+import InstructorCard from "../../Components/InstructorCard";
 
 function Instructors() {
+  const [instructors, setInstructors] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/instructor")
+      .then((res) => res.json())
+      .then((result) => setInstructors(result));
+  }, []);
   return (
     <div>
-        <PageTitle title="Instructors" image={cover}/>
-        <div className='grid grid-cols-1 lg:grid-cols-4 gap-6 w-full lg:w-3/4 my-10 px-6 lg:px-0 mx-auto'>
-            <InstructorCard/>
-            <InstructorCard/>
-            <InstructorCard/>
-            <InstructorCard/>
-            <InstructorCard/>
-            <InstructorCard/>
-        </div>
+      <PageTitle title="Instructors" image={cover} />
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6 w-full lg:w-3/4 my-10 px-6 lg:px-0 mx-auto">
+        {instructors.map((insturctor, index) => (
+          <InstructorCard key={insturctor} instructor={insturctor} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Instructors
+export default Instructors;
